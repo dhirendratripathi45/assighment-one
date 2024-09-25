48d2405dfefb776924592b496eeb6fff505d25e2
dhirendra <dktripathidhirendra76@gmail.com>
2024-09-26 00:02:17 +0545

    Assignment One - SL

---
+++ error.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Error</title>
+</head>
+<body>
+    <h2>Login Failed</h2>
+    <p>Please try again.</p>
+</body>
+</html>
+
---
+++ success.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Success</title>
+</head>
+<body>
+    <h2>Login Successful!</h2>
+    <p>Welcome,successfully.</p>
+</body>
+</html>
